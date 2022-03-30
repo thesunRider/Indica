8e32dbef5e82125fef4838e1e63bdc2aece92baf
suryasaradhi <suryasaradhi3@gmail.com>
2022-03-30 15:23:20 +0530

    Finished python flask backend

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+ind_left = 0
+ind_right = 0
+
+
+# 0 - left
+# 1 - right
+@app.route("/device_set")
+def device_on():
+	global ind_left
+	global ind_right
+
+	indic = int(request.args.get('indic'))
+	swtch = int(request.args.get('swtch'))
+
+	if indic == 0:
+		ind_left = swtch
+
+	elif indic == 1:
+		ind_right = swtch
+
+	print('ind left',ind_left)
+	print('ind right',ind_right)
+	
+	return '{"status":"ok"}'
+
+
+@app.route("/device_status")
+def device_status():
+	global ind_left
+	global ind_right
+
+	indic = int(request.args.get('indic'))
+
+	if indic == 0:
+		return str(int(ind_left))
+	
+	if indic == 1:
+		return str(int(ind_right))
+	
